ba6bc3ed28fb6f1bb2fff809fa35489236815829
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2021-04-13 09:20:03 -0400

    Update TeamsApi.cs

--- TeamsApi.cs
+++ TeamsApi.cs
@@ -8,7 +8,7 @@ namespace tk.pandapip1.hyperdrive
 {
     public static class TeamApi
     {
-        private static List<string> Teams = new List<string>();
+        internal static List<string> Teams = new List<string>();
         
         public static void CreateTeam(string guid)
         {
