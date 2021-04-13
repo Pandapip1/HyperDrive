67d85c14de40c6c2846e926deef5367df58fb60e
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2021-04-13 09:15:25 -0400

    Update RoleApi.cs

--- RoleApi.cs
+++ RoleApi.cs
@@ -9,18 +9,12 @@ namespace tk.pandapip1.hyperdrive
     public static class RoleApi
     {
         private static List<string> Roles = new List<string>();
-        private static List<string> Teams = new List<string>();
         private static Dictionary<byte, string> Pid2Role = new List<string>();
         private static Dictionary<byte, string> Role2Team = new List<string>();
         
         public static void CreateRole(string guid)
         {
             Roles.Add(guid);
-        }
-        
-        public static void CreateTeam(string guid)
-        {
-            Teams.Add(guid);
         }
         
         public static void AssignRoleToTeam(string role, string team)
@@ -36,18 +30,6 @@ namespace tk.pandapip1.hyperdrive
         {
             if (Pid2Role.ContainsKey(playerId)) return Pid2Role[playerId];
             return null;
-        }
-        
-        [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.HandleRpc))]
-        private static class RPCHandler
-        {
-            public static bool Prefix([HarmonyArgument(0)] byte callId, [HarmonyArgument(1)] MessageReader reader)
-            {
-                if (callId == 69)
-                {
-                    Pid2Role[reader.ReadString()] = reader.ReadByte();
-                }
-            }
         }
     }
 }
