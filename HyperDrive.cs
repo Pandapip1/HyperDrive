cb05ccaf9452d8e99181f96d27fedd845f0a9a56
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2021-04-19 09:57:01 -0400

    Update HyperDrive.cs

--- HyperDrive.cs
+++ HyperDrive.cs
@@ -6,30 +6,43 @@ using BepInEx;
 
 namespace tk.pandapip1.hyperdrive
 {
-    public static class RoleApi
+    public static class HyperDrive
     {
-        internal static List<string> Roles = new List<string>();
-        internal static Dictionary<byte, string> Pid2Role = new List<string>();
-        internal static Dictionary<byte, string> Role2Team = new List<string>();
+        internal static Dictionary<string, string> Roles = new Dictionary<string, string>();
+        internal static Dictionary<byte, string> Pid2Role = new Dictionary<string, string>();
+        
+        public static void CreateRole(string guid, string parent)
+        {
+            Roles[guid] = parent;
+        }
         
         public static void CreateRole(string guid)
         {
-            Roles.Add(guid);
+            Roles[guid] = "";
         }
         
-        public static void AssignRoleToTeam(string role, string team)
+        internal static void internal_AssignRoleToPlayer(byte pid, string role)
         {
-            Role2Team[role] = team;
+            Pid2Role[pid] = role;
         }
         
-        public static void AssignRoleToPlayer(string role, string team)
+        public static void AssignRoleToPlayer(byte pid, string role)
         {
+            internal_AssignRoleToPlayer(pid, role);
+            var messageWriter = AmongUsClient.Instance.StartRpc(PlayerControl.LocalPlayer.NetId, 69, SendOption.Reliable);
+            messageWriter.Write((byte) RPC.AssignRoleToPlayer);
+            messageWriter.EndMessage();
         }
         
-        public static string GetUserRole(byte playerId)
+        public static bool UserHasRole(byte playerId, string role)
         {
-            if (Pid2Role.ContainsKey(playerId)) return Pid2Role[playerId];
-            return null;
+            if (!Roles.ContainsKey(role)) return false;
+            if (role == "") return true;
+            while (role != ""){
+                if (Pid2Role.ContainsKey(playerId) && Pid2Role[playerId].StartsWith(role)) return true;
+                role = Roles[role];
+            }
+            return false;
         }
     }
 }
